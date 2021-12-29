d58dc0f8b64d3a875f3068ed9e0db319730c450e
Ariel Karlen <ariel.karlen@possumus.tech>
2021-12-29 12:24:02 -0300

    Armado de hoja de estilo. Video Layer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
+import Header from "../components/Commons/header";
+import VideoSlide from "../components/Home/videoSlide";
 
 export const Home = () => {
   return (
     <Fragment>
-      <h1>inicio</h1>
+      <Header />
+      <VideoSlide />
     </Fragment>
   );
 };
