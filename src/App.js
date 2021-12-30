28fe99870c07652b2c0c8c6224e1d097646e6777
Ariel Karlen <ariel.karlen@possumus.tech>
2021-12-30 14:27:53 -0300

    Menu y megamenu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 import AppRouter from "./routers/AppRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./css/layout.css";
 import Enviroment from "./components/Commons/enviroment";
 
 function App() {
