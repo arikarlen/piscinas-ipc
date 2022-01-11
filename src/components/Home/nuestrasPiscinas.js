c985ae6038d16b38decb0d90b5999109e729bc02
Ariel Karlen <ariel.karlen@possumus.tech>
2022-01-11 10:57:09 -0300

    Instalacion y configuracion de AOS para animaciones

--- src/components/Home/nuestrasPiscinas.js
+++ src/components/Home/nuestrasPiscinas.js
@@ -10,8 +10,11 @@ export const NuestrasPiscinas = () => {
     <Container fluid className="backsHome" id="nuestrasPiscinas">
       <Container>
         <Row>
-          <h1 className="text-center">Nuestras Piscinas</h1>
-          <h5 className="subtitulo text-center">
+          <h1 className="text-center" data-aos="zoom-in">
+            Nuestras Piscinas
+          </h1>
+
+          <h5 className="subtitulo text-center" data-aos="zoom-in">
             Contamos con 3 líneas adaptadas a tu necesidad y una amplia variedad
             de modelos.
           </h5>
@@ -19,7 +22,11 @@ export const NuestrasPiscinas = () => {
         <Row id="contentPiscinas" className="text-center">
           <Col lg={4}>
             <Link to="/page2">
-              <div className="cardsIpc">
+              <div
+                className="cardsIpc "
+                data-aos="zoom-in "
+                data-aos-offset="200"
+              >
                 <img src={logoPractipool}></img>
                 <p>
                   Ver modelos <i class="fas fa-chevron-right"></i>
@@ -30,7 +37,11 @@ export const NuestrasPiscinas = () => {
 
           <Col lg={4}>
             <Link to="/page2">
-              <div className="cardsIpc cardMedium">
+              <div
+                className="cardsIpc cardMedium"
+                data-aos="zoom-in"
+                data-aos-offset="300"
+              >
                 <img src={logoStylePool}></img>
                 <p>
                   Ver modelos <i class="fas fa-chevron-right"></i>
@@ -41,7 +52,11 @@ export const NuestrasPiscinas = () => {
 
           <Col lg={4}>
             <Link to="/page2">
-              <div className="cardsIpc">
+              <div
+                className="cardsIpc"
+                data-aos="zoom-in"
+                data-aos-offset="400"
+              >
                 <img src={logoLuxury}></img>
                 <p>
                   Ver modelos <i class="fas fa-chevron-right"></i>
