01e8d6dd74d155e35fa465a77a799172dda0bc43
Esteban Chiapa <esteban.chiapa@possumus.tech>
2022-01-17 18:59:26 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY_STAGING
 SERVER_FOLDER="front"
 
 #echo "Deploying to ${DEPLOY_SERVER}"
-scp -o StrictHostKeyChecking=no -r ../IPC-2022/  user-webserver-staging@${DEPLOY_SERVER}:/var/www/html/webserver-ipc/${SERVER_FOLDER}
+scp -o StrictHostKeyChecking=no -r ../ipc-2022/  user-webserver-staging@${DEPLOY_SERVER}:/var/www/html/webserver-ipc/${SERVER_FOLDER}
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null user-webserver-staging@${DEPLOY_SERVER} "cd /var/www/html/webserver-ipc/front/PRE-Deploy && ./deploy.sh"
