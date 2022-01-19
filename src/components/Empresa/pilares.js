097431ed144313b0e5c50411536a5c9e24fdf75c
luciana.rivier <luciana.rivier@possumus.tech>
2022-01-19 12:24:29 -0300

    Ajustes en la página empresa se temrina con la seccion de pilares

--- src/components/Empresa/pilares.js
+++ src/components/Empresa/pilares.js
@@ -4,51 +4,61 @@ import PeoplePool from "../../assets/banner-gente_01-min.jpg";
 
 export const Pilares = () => {
   return (
-    <Container id="section-pilares">
-      <Container>
+    <Container fluid id="section-pilares">
+      <Container className="padd-section">
         <Row>
-          <Col lg={6}>
+          <Col lg={6} className="col-titlepilares">
           <div className="columnInner">
-              <h2 className="title-left" data-aos="zoom-in" data-aos-offset="100">
-                ¡Hola! Somos IPC Pools
-              </h2>
+              <h1 className="title-left color-lightblue" data-aos="zoom-in" data-aos-offset="100">
+                Tres pilares
+                <h1 className="title-pilares color-blue" data-aos="zoom-in" data-aos-offset="110">que nos definen</h1>
+              </h1>
             </div>
           </Col>
 
           <Col lg={6}>
           <div className="columnInner">
-              <h3 className="title-pilares" data-aos="zoom-in" data-aos-offset="100">
-                ¡Hola! Somos IPC Pools
+            <div className="hover-section">
+              <h3 className="title-pilares" data-aos="zoom-in" data-aos-offset="200">
+                Innovación
               </h3>
               <p className="text-pilares" data-aos="zoom-in" data-aos-offset="200">
-              Nacimos en 1993 en Mendoza, Argentina. Somos líderes en la fabricación de piscinas de plástico reforzado con fibra de vidrio. A lo largo de casi tres décadas, nos convertimos en la empresa más grande de Argentina en su rubro.
-              </p>
-              <h3 className="title-pilares" data-aos="zoom-in" data-aos-offset="100">
-                ¡Hola! Somos IPC Pools
-              </h3>
-              <p className="text-pilares" data-aos="zoom-in" data-aos-offset="200">
-              Nacimos en 1993 en Mendoza, Argentina. Somos líderes en la fabricación de piscinas de plástico reforzado con fibra de vidrio. A lo largo de casi tres décadas, nos convertimos en la empresa más grande de Argentina en su rubro.
-              </p>
-              <h3 className="title-pilares" data-aos="zoom-in" data-aos-offset="100">
-                ¡Hola! Somos IPC Pools
-              </h3>
-              <p className="text-pilares" data-aos="zoom-in" data-aos-offset="200">
-              Nacimos en 1993 en Mendoza, Argentina. Somos líderes en la fabricación de piscinas de plástico reforzado con fibra de vidrio. A lo largo de casi tres décadas, nos convertimos en la empresa más grande de Argentina en su rubro.
+                Idear soluciones novedosas y originales ante necesidades del cliente, la organización y la sociedad.
               </p>
             </div>
+            <div className="hover-section">
+              <h3 className="title-pilares" data-aos="zoom-in" data-aos-offset="200">
+                Pasión
+              </h3>
+              <p className="text-pilares" data-aos="zoom-in" data-aos-offset="200">
+                Sentir entusiasmo y deseo por la excelencia.
+              </p>
+            </div>
+            <div className="hover-section">
+              <h3 className="title-pilares" data-aos="zoom-in" data-aos-offset="200">
+                Confianza
+              </h3>
+              <p className="text-pilares" data-aos="zoom-in" data-aos-offset="200">
+                Ser líderes en el rubro por el compromiso con nuestros clientes.
+              </p>
+            </div>
+          </div>
           </Col>
         </Row>
-       </Container>
-       <Row>
-       <Col lg={12}>
+      </Container>
+       <Container fluid className="backpilares">
+          <Row>
+            <Col lg={12}>
               <img
                 src={PeoplePool}
                 data-aos="zoom-in"
-                data-aos-offset="100"
+                data-aos-offset="210"
               ></img>
-       </Col>
-       </Row>
+            </Col>
+          </Row>
+        </Container>
     </Container>
+    
   );
 };
 
