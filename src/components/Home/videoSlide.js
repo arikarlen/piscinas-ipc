a64970f7920a888bc642c7bfdc5300c6a2d738e3
luciana.rivier <luciana.rivier@possumus.tech>
2022-01-31 10:14:51 -0300

    Ajuste home slider video

--- src/components/Home/videoSlide.js
+++ src/components/Home/videoSlide.js
@@ -7,7 +7,7 @@ export const VideoSlide = () => {
       <div className="video-responsive">
         <iframe
           id="videoContainer"
-          src="https://www.youtube.com/embed/Qy7HrO0s6vc?rel=0&amp;autoplay=0&mute=1&showinfo=0&controls=0&loop=1&playlist=Qy7HrO0s6vc&modestbranding=1"
+          src="https://www.youtube.com/embed/Qy7HrO0s6vc?rel=0&amp;autoplay=1&mute=1&showinfo=0&controls=0&loop=1&playlist=Qy7HrO0s6vc&modestbranding=1"
           frameBorder="0"
           allowFullScreen
           title="video"
