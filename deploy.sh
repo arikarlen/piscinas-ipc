5f35b11fd87d26de342deb039722675115605793
root <root@vm-IPC-Develop.xau2f3a4mdhevbsskh5azzispf.bx.internal.cloudapp.net>
2022-03-22 13:43:15 +0000

    delete files ci/cd

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DEPLOY_SERVER=$DEPLOY_SERVER_DEVELOP
-SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY_DEVELOP
-SERVER_FOLDER="front"
-
-#Ejecución remota de script en VM para deploys continuos
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null user-ipcdevelop@${DEPLOY_SERVER} "cd /var/www/html/webserver-ipc/front/PRE-Deploy && ./deploy.sh"
