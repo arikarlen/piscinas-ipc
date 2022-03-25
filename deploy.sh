1d9f272f4abf5a3247f4b479fd1cc6bc485e090c
Lucas Fernández <lucas.fernandez@possumus.tech>
2022-03-25 18:49:17 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DEPLOY_SERVER=$DEPLOY_SERVER_STAGING
-SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY_STAGING
+DEPLOY_SERVER=$DEPLOY_SERVER_PROD
+SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY_PROD
 SERVER_FOLDER="front"
 
 #Ejecución remota de script para deploys automáticos
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null user-ipcstaging@${DEPLOY_SERVER} "cd /var/www/html/webserver-ipc/front/PRE-Deploy && ./deploy.sh"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ipc-user-prod@${DEPLOY_SERVER} "cd /var/www/html/webserver-ipc/front/PRE-Deploy && ./deploy.sh"
